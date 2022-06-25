99707055291dfbc47b48ba3570c4e2225229e9e4
Jisan <66010486+Jisan09@users.noreply.github.com>
2022-06-26 01:42:55 +0530

    improve

--- vc_manager.py
+++ vc_manager.py
@@ -1,23 +1,31 @@
+from telethon import functions
+from telethon.errors import ChatAdminRequiredError, UserAlreadyInvitedError
+from telethon.tl.types import Channel, Chat, User
 from userbot import catub
 from userbot.core.managers import edit_delete, edit_or_reply
 from userbot.helpers.utils import mentionuser
-from telethon.tl.types import User, Channel, Chat
-from telethon import functions
-from telethon.errors import ChatAdminRequiredError, UserAlreadyInvitedError
 
 plugin_category = "extra"
 
 
 async def get_group_call(chat):
     if isinstance(chat, Channel):
-        result = await catub(functions.channels.GetFullChannelRequest(
-            channel=chat
-        ))
+        result = await catub(functions.channels.GetFullChannelRequest(channel=chat))
     elif isinstance(chat, Chat):
-        result = await catub(functions.messages.GetFullChatRequest(
-            chat_id=chat.id
-        ))
+        result = await catub(functions.messages.GetFullChatRequest(chat_id=chat.id))
     return result.full_chat.call
+
+
+async def chat_vc_checker(event, chat, edits=True):
+    if isinstance(chat, User):
+        await edit_delete(event, "Voice Chats are not available in Private Chats")
+        return None
+    result = await get_group_call(chat)
+    if not result:
+        if edits:
+            await edit_delete(event, "No Group Call in this chat")
+        return None
+    return result
 
 
 async def parse_entity(entity):
@@ -27,70 +35,60 @@ async def parse_entity(entity):
 
 
 @catub.cat_cmd(
-    pattern="gcstart",
-    command=("gcstart", plugin_category),
+    pattern="vcstart",
+    command=("vcstart", plugin_category),
 )
 async def start_vc(event):
     "To start a Voice Chat."
-    chat = event.chat_id
-    await edit_or_reply(event, 'Starting Group Call')
-    vc_chat = await catub.get_entity(chat)
-    if isinstance(chat, User):
-        return await edit_delete(event, "Voice Chats are not available in Private Chats")
-
-    gc_call = await get_group_call(vc_chat)
-
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat, False)
     if gc_call:
-        return await edit_delete(event, 'Group Call is already available in this chat')
-
+        return await edit_delete(event, "Group Call is already available in this chat")
     try:
-        await catub(functions.phone.CreateGroupCallRequest(
-            peer=vc_chat,
-            title='Cat VC',
-        ))
-        await edit_delete(event, f'Started Group Call')
+        await catub(
+            functions.phone.CreateGroupCallRequest(
+                peer=vc_chat,
+                title="Cat VC",
+            )
+        )
+        await edit_delete(event, "Started Group Call")
     except ChatAdminRequiredError:
         await edit_delete(event, "You should be chat admin to start vc", time=20)
 
 
 @catub.cat_cmd(
-    pattern="gcend",
-    command=("gcend", plugin_category),
+    pattern="vcend",
+    command=("vcend", plugin_category),
 )
 async def end_vc(event):
     "To end a Voice Chat."
-    chat = event.chat_id
-    await edit_or_reply(event, 'Ending Group Call')
-
-    vc_chat = await catub.get_entity(chat)
-    if isinstance(chat, User):
-        return await edit_delete(event, "Voice Chats are not available in Private Chats")
-
-    gc_call = await get_group_call(vc_chat)
-
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
+        return
     try:
-        await catub(functions.phone.DiscardGroupCallRequest(
-            call=gc_call
-        ))
-        await edit_delete(event, f'Group Call Ended')
+        await catub(functions.phone.DiscardGroupCallRequest(call=gc_call))
+        await edit_delete(event, "Group Call Ended")
     except ChatAdminRequiredError:
-        await edit_delete(event, "You should be chat admin to start vc", time=20)
+        await edit_delete(event, "You should be chat admin to kill vc", time=20)
 
 
 @catub.cat_cmd(
-    pattern="gcinv ?(.*)?",
-    command=("gcinv", plugin_category),
+    pattern="vcinv ?(.*)?",
+    command=("vcinv", plugin_category),
 )
 async def inv_vc(event):
     "To invite users to vc."
-    chat = event.chat_id
     users = event.pattern_match.group(1)
-    await edit_or_reply(event, 'Inviting User to Group Call')
+    if not users:
+        return await edit_delete("Whom Should i invite")
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat)
+    if not gc_call:
+        return
+    await edit_or_reply(event, "Inviting User to Group Call")
     if users:
-        entities = users.split(' ')
+        entities = users.split(" ")
         users = []
         for entity in entities:
             cc = await parse_entity(entity)
@@ -99,53 +97,27 @@ async def inv_vc(event):
     elif event.reply_to_msg_id:
         reply = await event.get_reply_message()
         users = [reply.from_id]
-
-    if not users:
-        return await edit_delete('Whom Should i invite')
-
     try:
-        vc_chat = await catub.get_entity(chat)
-    except Exception as e:
-        return await edit_delete(event, f'ERROR : \n{e or "UNKNOWN CHAT"}')
-
-    gc_call = await get_group_call(vc_chat)
-
-    if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
-    try:
-        await catub(functions.phone.InviteToGroupCallRequest(
-            call=gc_call,
-            users=users
-        ))
-        await edit_delete(event, f'Invited users to Group Call')
+        await catub(functions.phone.InviteToGroupCallRequest(call=gc_call, users=users))
+        await edit_delete(event, "Invited users to Group Call")
     except UserAlreadyInvitedError:
         return await edit_delete(event, "User is Already Invited", time=20)
 
 
 @catub.cat_cmd(
-    pattern="gcinfo",
-    command=("gcinfo", plugin_category),
+    pattern="vcinfo",
+    command=("vcinfo", plugin_category),
 )
-async def inv_vc(event):
+async def info_vc(event):
     "Get info of VC."
-    chat = event.chat_id
-    await edit_or_reply(event, 'Getting Group Call Info')
-    try:
-        vc_chat = await catub.get_entity(chat)
-    except Exception as e:
-        return await edit_delete(event, f'ERROR : \n{e or "UNKNOWN CHAT"}')
-
-    gc_call = await get_group_call(vc_chat)
-
+    await edit_or_reply(event, "Getting Group Call Info")
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
-    call_details = await catub(functions.phone.GetGroupCallRequest(
-        call=gc_call,
-        limit=1
-    ))
-
+        return
+    call_details = await catub(
+        functions.phone.GetGroupCallRequest(call=gc_call, limit=1)
+    )
     grp_call = "**Group Call Info**\n\n"
     grp_call += f"**Title :** {call_details.call.title}\n"
     grp_call += f"**Participants Count :** {call_details.call.participants_count}\n\n"
@@ -159,44 +131,42 @@ async def inv_vc(event):
 
 
 @catub.cat_cmd(
-    pattern="gctitle?(.*)?",
-    command=("gctitle", plugin_category),
+    pattern="vctitle?(.*)?",
+    command=("vctitle", plugin_category),
 )
-async def inv_vc(event):
+async def title_vc(event):
     "To change vc title."
-    chat = event.chat_id
     title = event.pattern_match.group(1)
-    await edit_or_reply(event, 'Changing Group Call Title')
+    await edit_or_reply(event, "Changing Group Call Title")
     if not title:
         return await edit_delete("What should i keep as title")
-
-    vc_chat = await catub.get_entity(chat)
-    if isinstance(chat, User):
-        return await edit_delete(event, "Voice Chats are not available in Private Chats")
-
-    gc_call = await get_group_call(vc_chat)
-
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat)
     if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
-    await catub(functions.phone.EditGroupCallTitleRequest(
-        call=gc_call,
-        title=title
-    ))
+        return
+    await catub(functions.phone.EditGroupCallTitleRequest(call=gc_call, title=title))
     await edit_delete(event, f"VC title was changed to **{title}**")
 
 
 @catub.cat_cmd(
-    pattern="gcmute ?(.*)?",
-    command=("gcmute", plugin_category),
+    pattern="vc(|un)mute ([\s\S]*)",
+    command=("vcmute", plugin_category),
 )
 async def mute_vc(event):
     "To mute users in vc."
-    chat = event.chat_id
-    users = event.pattern_match.group(1)
-    await edit_or_reply(event, 'Muting User in Group Call')
+    cmd = event.pattern_match.group(1)
+    users = event.pattern_match.group(2)
+    check = "Unmute" if cmd else "Mute"
+    if not users:
+        return await edit_delete(f"Whom Should i {check}")
+    vc_chat = await catub.get_entity(event.chat_id)
+    gc_call = await chat_vc_checker(event, vc_chat)
+    if not gc_call:
+        return
+
+    await edit_or_reply(event, f"{check[:-1]}ing User in Group Call")
     if users:
-        entities = users.split(' ')
+        entities = users.split(" ")
         users = []
         for entity in entities:
             cc = await parse_entity(entity)
@@ -206,63 +176,19 @@ async def mute_vc(event):
         reply = await event.get_reply_message()
         users = [reply.from_id]
 
-    if not users:
-        return await edit_delete('Whom Should i mute')
-
-    vc_chat = await catub.get_entity(chat)
-    if isinstance(chat, User):
-        return await edit_delete(event, "Voice Chats are not available in Private Chats")
-
-    gc_call = await get_group_call(vc_chat)
-
-    if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
     for user in users:
-        await catub(functions.phone.EditGroupCallParticipantRequest(
-            call=gc_call,
-            participant=user,
-            muted=True,
-        ))
-    await edit_delete(event, f'Muted users in Group Call')
+        await catub(
+            functions.phone.EditGroupCallParticipantRequest(
+                call=gc_call,
+                participant=user,
+                muted=bool(not cmd),
+            )
+        )
+    await edit_delete(event, f"{check}d users in Group Call")
 
 
 @catub.cat_cmd(
-    pattern="gcunmute ?(.*)?",
-    command=("gcunmute", plugin_category),
+    command=("vcunmute", plugin_category),
 )
 async def unmute_vc(event):
     "To unmute users in vc."
-    chat = event.chat_id
-    users = event.pattern_match.group(1)
-    await edit_or_reply(event, 'Unmuting User in Group Call')
-    if users:
-        entities = users.split(' ')
-        users = []
-        for entity in entities:
-            cc = await parse_entity(entity)
-            if isinstance(cc, User):
-                users.append(cc)
-    elif event.reply_to_msg_id:
-        reply = await event.get_reply_message()
-        users = [reply.from_id]
-
-    if not users:
-        return await edit_delete('Whom Should i unmute')
-
-    vc_chat = await catub.get_entity(chat)
-    if isinstance(chat, User):
-        return await edit_delete(event, "Voice Chats are not available in Private Chats")
-
-    gc_call = await get_group_call(vc_chat)
-
-    if not gc_call:
-        return await edit_delete(event, 'No Group Call in this chat')
-
-    for user in users:
-        await catub(functions.phone.EditGroupCallParticipantRequest(
-            call=gc_call,
-            participant=user,
-            muted=False,
-        ))
-    await edit_delete(event, f'Unmuted users in Group Call')
