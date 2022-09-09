c108b203314c7d653c31b9229f66e53589a17fd0
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-09-09 20:41:22 +0300

    Update stream_helper.py

--- helper/stream_helper.py
+++ helper/stream_helper.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 from requests.exceptions import MissingSchema
 from requests.models import PreparedRequest
-from sbb_b.utils import runcmd
+from jepthon.utils import runcmd
 from yt_dlp import YoutubeDL
 
 
